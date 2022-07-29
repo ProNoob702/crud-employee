2069d4eaf9f9fc9cdaa9ab9443d4ec1df5511d4d
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-07-29 20:19:36 +0100

    setup project

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace crud_employee.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void RegisterServices(this IServiceCollection collection)
+        {
+           
+        }
+
+    }
+}
